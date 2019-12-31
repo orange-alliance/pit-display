06bf573c41b4898083a7042585cde5427e13dcb5
Ofek Ashery <ofek.ashery@gmail.com>
2019-12-31 21:55:22 +0200

    Improve URLs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PitDisplay from './PitDisplay';
 import './index.css';
 
-const eventKey = (window.location.pathname.substr(1) || 'null');
+let eventKey = (window.location.pathname.replace(/\//g,'') || 'null');
+if (eventKey.split('-').length == 2) eventKey = '1920-' + eventKey;
 
 const App: React.FC = () => {
   return <PitDisplay eventKey={eventKey} />;
