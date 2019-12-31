745123854b02a02332b578916a6dad73016b1d67
Ofek Ashery <ofek.ashery@gmail.com>
2019-12-31 21:55:42 +0200

    Typo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PitDisplay from './PitDisplay';
 import './index.css';
 
 let eventKey = (window.location.pathname.replace(/\//g,'') || 'null');
-if (eventKey.split('-').length == 2) eventKey = '1920-' + eventKey;
+if (eventKey.split('-').length === 2) eventKey = '1920-' + eventKey;
 
 const App: React.FC = () => {
   return <PitDisplay eventKey={eventKey} />;
